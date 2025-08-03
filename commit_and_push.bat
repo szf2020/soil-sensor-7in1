35dc8d7cfc052ca13bd6e9c7f921edbc507a6975
user <user@example.com>
2025-08-03 14:33:39 +0300

    cleanup: удален временный batch файл

--- commit_and_push.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Создание коммита точки отката...
-git add .
-git commit -m "feat: комплексная научная валидация всех алгоритмов JXCT - точка отката"
-echo Пуш в репозиторий...
-git push origin refactoring
-echo Готово!
-pause 
